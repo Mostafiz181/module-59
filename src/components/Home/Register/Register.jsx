63a4fbf0497af880361d2cf10912bb85c7de7716
mostafiz181 <dev.mostafizurrahman@gmail.com>
2023-04-16 15:30:53 +0600

    class-4 get email and password from user and show work done

--- src/components/Home/Register/Register.jsx
+++ src/components/Home/Register/Register.jsx
@@ -1,4 +1,9 @@
 import React, { useState } from 'react';
+import "./Register.css"
+import {createUserWithEmailAndPassword, getAuth} from "firebase/auth"
+import app from '../../../firebase/firebase.config';
+
+const auth = getAuth(app)
 
 
 
@@ -18,17 +23,23 @@ const Register = () => {
         event.preventDefault();
         const email = event.target.email.value;
         const password = event.target.password.value;
-
-        console.log(email,password)
+        console.log(email,password);
+        createUserWithEmailAndPassword(auth,email,password)
+        .then(result=>{
+            const loggedUser =result.user;
+            console.log(loggedUser)
+        }).catch(error=>{
+            console.log(error)
+        })
 
     }
     return (
         <div className='container'>
             <h2>Please Register</h2>
             <form onSubmit={handleSubmit} >
-                <input onChange={handleEmailChange} type="email" name="email" id="email" placeholder='Input Your Email'/> <br />
-                <input onBlur={handlePassword} type="password" name="password" id="password" placeholder='Input Your Password'/> <br />
-                <input type="submit" value="Register" />
+                <input className='Input-field' onChange={handleEmailChange} type="email" name="email" id="email" placeholder='Input Your Email'/> <br />
+                <input className='Input-field' onBlur={handlePassword} type="password" name="password" id="password" placeholder='Input Your Password'/> <br />
+                <input className='btn-register' type="submit" value="Register" />
             </form>
         </div>
     );
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAa2iPQIOPqQ0ZcN1bFg4FRAOkshKRMqr4",
+  authDomain: "email-password-auth-65f85.firebaseapp.com",
+  projectId: "email-password-auth-65f85",
+  storageBucket: "email-password-auth-65f85.appspot.com",
+  messagingSenderId: "203591523250",
+  appId: "1:203591523250:web:6871d0ae116430f8af62ab"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
