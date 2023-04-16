c37a2b67ef7244e832458db4baa2a02a72b651ba
mostafiz181 <dev.mostafizurrahman@gmail.com>
2023-04-16 12:01:08 +0600

    video 2 addeded

--- src/components/Home/Register/Register.jsx
+++ src/components/Home/Register/Register.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+
 
 const Register = () => {
+
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+    const handleEmailChange =(event)=>{ 
+        setEmail(event.target.value)
+    
+    }
+    const handlePassword =(event)=>{
+        setPassword(event.target.value)
+    }
+
+    const handleSubmit =(event)=>{
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+        console.log(email,password)
+
+    }
     return (
         <div>
             <h2>Please Register</h2>
-            <form >
-                <input type="email" name="email" id="email" placeholder='Input Your Email'/> <br />
-                <input type="password" name="password" id="password" placeholder='Input Your Password'/> <br />
+            <form onSubmit={handleSubmit} >
+                <input onChange={handleEmailChange} type="email" name="email" id="email" placeholder='Input Your Email'/> <br />
+                <input onBlur={handlePassword} type="password" name="password" id="password" placeholder='Input Your Password'/> <br />
                 <input type="submit" value="Register" />
             </form>
         </div>
